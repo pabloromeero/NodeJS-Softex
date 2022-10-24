5d4c2838072e7b882dbe53bf605baf5aa1b886c0
Pablo Romero <pablo10romero10@gmail.com>
2022-10-24 10:27:47 -0300

    atualizado rotas

--- Exercicios/rotas/rotas.js
+++ Exercicios/rotas/rotas.js
@@ -4,13 +4,25 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('<h1>Página Inicial</h1>')
 })
-app.get('/contato/linkedin/', (req, res) => {
-    res.send('<h2>https://www.linkedin.com/in/pablo-romero-50599a17b/</h2>')
+app.get('/iniciar', (req, res) => {
+    res.status(404).send('Caminho não encontrado!')
 })
+app.get('/user', function (req, res) {
+    res.send('Lista de usuários');
+  });
+   
+app.post('/user/insert', function (req, res) {
+    res.send('Inserir um usuário');
+  });
+   
+app.put('/user/:id', function (req, res) {
+    res.send(`Editar um usuário com o id ${req.params.id}`);
+  });
+   
+app.delete('/user/:id', function (req, res) {
+    res.send(`Excluir um usuário com o id ${req.params.id}`);
+  });
 
-app.get('/contato/github', (req, res) => {
-    res.send('<h2>https://github.com/pabloromeero</h2>')
-})
 app.listen(8080, () => {
     console.log('Servidor rodando na URL http://localhost:8080')
 });
